c3110c996c6a2eca1aab1554b6b4a4c26d1df921
Alex Sinishin <ajsinishin@si4b.com>
2019-01-20 02:48:16 +0800

    Update Gemfile.lock for Heroku

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -15,7 +15,7 @@ describe "Creating a new movie" do
         fill_in "Cast", with: "The award-winning cast"
         fill_in "Director", with: "The ever-creative director"
         fill_in "Duration", with: "123 min"
-        fill_in "Image file name", with: "movie.png"
+        fill_in "Image file name", with: "placeholder.png"
 
         select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
 
